41514c16680eec2947b97f321fe49d3c2bbdec3c
Robert Mulder <rmmulder@gmail.com>
2020-09-23 23:07:21 -0500

    Save updated working code state

--- src/App.js
+++ src/App.js
@@ -2,28 +2,23 @@ import React from 'react';
 import './App.scss';
 import Carousel from 'react-bootstrap/Carousel';
 
-
-function Slides(product) {
+const Slides = (product) => {
   const [activeIndex, setActiveIndex] = React.useState(0);
   const myInterval = 5000;
   const wrap = false;
-  let autoKey = 1;
+  const slide = false;
 
-  function handleSelect(selectedIndex) {
-    // console.log(`activeIndex: `, activeIndex);
-    // console.log(`selectedIndex: `, selectedIndex);
+  const handleSelect = (selectedIndex) => {
     setActiveIndex(selectedIndex);
   }
 
   product = product.product;
-  console.log(`product: `, product);
-  console.log(`slides: `, product.slides);
-  console.log(`activeIndex: `, activeIndex);
   return (
     <Carousel
       activeIndex={activeIndex}
       interval={myInterval}
       wrap={wrap}
+      slide={slide}
       onSelect={handleSelect}
     >
       {product.slides?.map((slide) => (
@@ -40,125 +35,14 @@ function Slides(product) {
     </Carousel>
   )
 }
-/*
-      <Carousel.Item key={autoKey++}>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/800/400?text=First slide&bg=373940"
-          alt="First slide"
-          width="363"
-          height="363"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item key={autoKey++}>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/800/400?text=Second slide&bg=282c34"
-          alt="Third slide"
-          width="363"
-          height="363"
-        />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item key={autoKey++}>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/800/400?text=Third slide&bg=20232a"
-          alt="Third slide"
-          width="363"
-          height="363"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-
-						<div uib-carousel active="active" interval="myInterval" no-wrap="noWrapSlides">
-							<div uib-slide ng-repeat="slide in product.slides track by slide.id" index="slide.id">
-								<img ng-src="{{slide.image}}" style="margin:auto;"  width="{{product.hero.width}}" height="{{product.hero.height}}"/>
-							</div>
-						</div>
-
-
-    <Carousel
-      activeIndex={activeIndex}
-      interval={myInterval}
-      wrap={wrap}
-    >
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/800/400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/800/400?text=Second slide&bg=282c34"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://picsum.photos/800/400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-    <Carousel activeIndex={activeIndex} interval={myInterval} wrap={wrap}>
-      {product.slide?.map((slide) => (
-        <Slide product={product} slide={slide} key={slide.id} />
-      ))}
-    </Carousel>
-
-    <div uib-carousel active="active" interval="myInterval" no-wrap="noWrapSlides">
-      {product.slide?.map((slide) => (
-        <Slide product={product} slide={slide} key={slide.id} />
-      ))}
-    </div>
-*/
-
-function Products() {
+const Products = () => {
   const [products, setProducts] = React.useState(
     () => window.localStorage.getItem('products') || [],
   )
 
   React.useEffect(() => {
     fetchProducts().then((response) => {
-      console.log(`=================> products: `, response);
-
       let products = [];
       response.groups.forEach((product) => {
         product.slides = [];
@@ -194,10 +78,8 @@ function Products() {
       .then(response => JSON.parse(response));
   }
 
-  // products = fetchProducts();
-  // console.log(`=================> products: `, products);
   return (
-    <div>
+    <div className="row">
       {products?.groups?.map((product) => (
         <Product product={product} key={product.id} />
       ))}
@@ -205,14 +87,15 @@ function Products() {
   );
 }
 
-function createMarkup(html) { return { __html: html }; };
+const createMarkup = (html) => { return { __html: html }; };
 
-function Product({product}) {
+const Product = ({product}) => {
   const [isToggled, setIsToggled] = React.useState(false);
 
-  function toggleCarousel () {
+  // console.log(`product: `, product);
+  const toggleCarousel = () => {
     product.isToggled = !product.isToggled;
-    setIsToggled(!product.isToggled);
+    setIsToggled(product.isToggled);
   }
 
   return (
@@ -221,10 +104,13 @@ function Product({product}) {
           width={product.hero.width} height={product.hero.height} />
         <h2 className="product-name" dangerouslySetInnerHTML={createMarkup(product.name)}></h2>
         <div className="product-price">$
-          {product.priceRange?.type === 'sale' && <span>{ product.priceRange?.selling?.low }</span>}
-          {product.priceRange?.type !== 'sale' && <span>{ product.priceRange?.regular?.low }</span>}
+          {(!product.priceRange?.type || product.priceRange?.type === 'sale') ? <span>{product.priceRange?.selling?.low}</span> : null}
+          {product.priceRange?.type !== 'sale' ? <span>{product.priceRange?.regular?.low}</span> : null }
+          {(!product.price?.type || product.price?.type === 'sale') ? <span>{product.price?.selling?.low}</span> : null }
+          {(product.price?.type !== 'sale' && product.price?.type !== 'special') ? <span>{product.price?.regular.toFixed(2)}</span> : null }
+          {(product.price?.type === 'sale' || product.price?.type === 'special') ? <span>{product.price?.selling.toFixed(2)}</span> : null}
         </div>
-        <div id={product.id + '_carousel'} className={`carousel ${product.isToggled ? "" : "hidden"}`}>
+        <div id={product.id + '_carousel'} className={`carousel ${isToggled ? "" : "hidden"}`}>
           <button type="button" className="close close-button" onClick={toggleCarousel} aria-label="Close">
 						<span aria-hidden="true" className="cross-symbol">&times;</span>
 					</button>
@@ -241,12 +127,10 @@ function Product({product}) {
   );
 }
 
-function App() {
+const App = () => {
   return (
     <div className="App container">
-      <div className="row">
-        <Products />
-      </div>
+      <Products />
     </div>
   );
 }
